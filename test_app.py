c074dec99da0c655034ef465985a234f7b4632fb
aherzfeld <herzfeld.andrew@gmail.com>
2018-12-05 17:28:57 +1100

    added tests for Verb._compose & Verb._decompose

--- test_app.py
+++ test_app.py
@@ -3,7 +3,23 @@ import Verb  # not yet built
 
 
 class TestVerb(unittest.TestCase):
-    
+
+    def test_compose(self):
+        self.assertEqual(Verb._compose('ㄱ', 'ㅏ'), '가')
+        self.assertEqual(Verb._compose('ㅎ', 'ㅏ', 'ㄴ'), '한')
+        # checking if separate chars work as jonseong
+        self.assertEqual(Verb._compose('ㅇ', 'ㅣ', 'ㄹㄱ'), '읽')
+        self.assertEqual(Verb._compose('ㅇ', 'ㅣ', 'ㄺ'), '읽')
+        self.assertEqual(Verb._compose('ㅎ', 'ㅐ'), '해')
+
+    def test_decompose(self):
+        self.assertEqual(Verb._decompose('가'), 'ㄱ', 'ㅏ')
+        self.assertEqual(Verb._decompose('한'), 'ㅎ', 'ㅏ', 'ㄴ')
+        # checking if separate chars work as jonseong
+        self.assertEqual(Verb._decompose('읽'), 'ㅇ', 'ㅣ', 'ㄹㄱ')
+        self.assertEqual(Verb._decompose('읽'), 'ㅇ', 'ㅣ', 'ㄺ')
+        self.assertEqual(Verb._decompose('해'), 'ㅎ', 'ㅐ')
+
     # check object instatiation depending on implementation of Verb
     # might need to be Verb([verb]).conjugate_present
     def test_conjugate_present_tense(self):
