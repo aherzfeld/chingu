b5ae5a8bcedaabfac3750e47c38bf487c49905af
aherzfeld <herzfeld.andrew@gmail.com>
2018-12-10 18:25:31 +1100

    Created VerbQuiz subclass

--- chingu/quiz.py
+++ chingu/quiz.py
@@ -1,5 +1,8 @@
-# Quiz parent class
+# Quiz parent class & subject-specific quiz sub-classes
+import random
 from abc import ABCMeta, abstractmethod
+from chingu.verb import Verb
+from chingu.verblist import verb_list
 
 
 class Quiz(object):
@@ -15,14 +18,26 @@ class Quiz(object):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, quiz_length=10):
+    def __init__(self, subject_dict, quiz_length=10):
+        self.subject_dict = subject_dict
         self.quiz_length = quiz_length
         self.num_correct = 0
         self.num_wrong = 0
 
+    @property
     @abstractmethod
     def quiz_type():
         """ Return a string representing what type of quiz this is """
+        pass
+
+    @abstractmethod
+    def load_questions(subject_dict):
+        """ Returns n(quiz_length) questions/answers as list of tuples """
+        pass
+
+    @abstractmethod
+    def create_question(question_answer_tuple):
+        """ Receives question/answer tuple and returns formatted string """
         pass
 
     @property
@@ -39,5 +54,50 @@ class Quiz(object):
 
     # TODO: get_random_item from provided dict (verbs, phrases etc)
     # Maybe this would be implemented in a Dict class??
+
+    #TODO: start_quiz - loop through questions until done
+
+
+class VerbQuiz(Quiz, Verb):
+    """ Builds verb quiz logic on top of Quiz parent class
+
+    Param: quiz_length - how many questions the quiz will be
+    Param: subject_dict - always verb_dict in the case of VerbQuiz
+    """
+
+    def __init__(self, subject_dict, quiz_length=10):
+        super().__init__(subject_dict, quiz_length=10)
+        # TODO: load n(quiz_length) question/answers randomly from subject_dict
+        # think about abstracting some of this to Quiz class
+        key_dict = list(subject_dict)
+        random_keys = random.sample(key_dict, quiz_length)
+        # create list of question/answer tuples
+        # specific to present tense for now - add other options later
+        self.question_answers = [
+            (q, self.conjugate_present(q), subject_dict[q]) for q in random_keys]
+
+    def create_question(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
