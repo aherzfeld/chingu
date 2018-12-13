9ef422583893de796f83424b755d3daa6d1aeded
aherzfeld <herzfeld.andrew@gmail.com>
2018-12-13 18:40:32 +1100

    Created main module

--- chingu/app.py
+++ chingu/app.py
@@ -1,6 +1,23 @@
-import sys
-import verb
-import verblist
+import quiz
 
 
-print('Welcome to Chingu, your Korean study buddy. ^^')
+print('\nWelcome to Chingu, your Korean study buddy. ^^\n')
+
+# instantiate default VerbQuiz - quiz_type: present, quiz_length: 10
+verbquiz = quiz.VerbQuiz(quiz.verb_list, quiz_length=5)
+
+quiz_data = verbquiz.quiz_data
+quiz_type = verbquiz.quiz_type
+
+
+io = quiz.QuizInterface(quiz_data, quiz_type)
+
+results = io.start_quiz()
+
+print('\nThank you for trying Chingu!\n')
+
+print('You completed a {} tense verb quiz at {}.\n\n\
+You got {} questions correct and {} questions wrong.\n'.format(results[0],
+    results[3], results[1], results[2]))
+
+print('We hope you come back soon!\n')
