31685419105774c358baa756ec6682924acd7035
aherzfeld <herzfeld.andrew@gmail.com>
2018-12-24 17:59:17 +0800

    refactored QuizInterface class

--- chingu/quiz.py
+++ chingu/quiz.py
@@ -1,7 +1,7 @@
 # Quiz parent class & subject-specific quiz sub-classes
 import random
 from datetime import datetime
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 from chingu.verb import Verb
 from chingu.verblist import verb_dict
 
@@ -37,7 +37,6 @@ class Question():
             return '\nWhat is the {} tense form of {}?'.format(
                 quiz_type, key)
 
-    # currently in QuizInterface
     def check(self, user_answer):
         """ Return True if user input answer is correct """
 
@@ -62,9 +61,11 @@ class Quiz(object):
     Attributes:
         type - category specific subtype (i.e. 'definition')
         type_method - algorithm to generate answer from key based on type
-        length - number of questions quiz will test (param: quiz_length) 
+        length - number of questions quiz will test (param: quiz_length)
         key_list - (quiz_length) random keys from dictionary
         question_list - (quiz_length) list of Question objects
+        num_correct - number of questions answered correctly
+        num_wrong - number of questions answered incorrectly
     """
 
     __metaclass__ = ABCMeta
@@ -82,9 +83,18 @@ class Quiz(object):
         # add underscore to make private??
         self.key_list = self.make_key_list
         self.question_list = self.make_question_list
+        self.num_correct = 0
+        self.num_wrong = 0
+        self.date_taken = None
 
+    # can you string formatting for title case
     def __str__(self):
-        pass
+        """ Return quiz_string for printing based on quiz_specs """
+
+        if self.type == 'definition':
+            return f'{self.category} {self.type} quiz'
+        else:
+            return f'{self.type} tense {self.category} quiz'
 
     @property
     def make_key_list(self):
@@ -92,13 +102,35 @@ class Quiz(object):
         key_list = list(self.dictionary)
         return random.sample(key_list, self.length)
 
-    # implement this fully here , remove from VerbQuiz
     @property
     def make_question_list(self):
         """ Returns list of Question objects - one for each key in key_list """
 
         return [Question(k, self.type_method(k), self.dictionary[k], self.type)
                 for k in self.key_list]
+
+    @property
+    def questions_asked(self):
+        return self.num_correct + self.num_wrong
+
+    @property
+    def questions_remaining(self):
+        return self.length - (self.num_correct + self.num_wrong)
+
+    @property
+    def score_percent(self):
+        return round(self.num_correct / self.questions_asked, 2)
+
+    # can this be better implemented with setters, getters?
+    def update_score(self, boolean):
+        """ Receives Boolean, updates num_correct(True) / num_wrong(False) """
+        if not isinstance(boolean, bool):
+            return False
+        if boolean:
+            self.num_correct += 1
+        elif not boolean:
+            self.num_wrong += 1
+        return True
 
 
 class VerbQuiz(Quiz, Verb):
@@ -131,26 +163,12 @@ class VerbQuiz(Quiz, Verb):
         super().__init__(quiz_type, quiz_length)
 
 
-    # DELETE THIS AFTER RUNNING TESTS
-    # @property
-    # def quiz_data(self):
-    #     """ Returns - [(question_key, answer, definition, question_str), ...]]
-
-    #     Utilizes: dictionary - subject specific dictionary {word: definition}
-    #               conjugate_present(verb) - method from Verb class
-    #               _question_keys - list of keys for dictionary from Quiz
-    #               option_method - method corresponding to quiz_option """
-
-    #     return [(q, self.type_method(q), self.dictionary[q],
-    #              self._question(q)) for q in self.key_list]
-
-
 class QuizSetup():
     """ Gathers user input to instantiate quiz """
 
     # NounQuiz not yet implemented
     categories = {'verb': (VerbQuiz, verb_dict),
-                  'noun': ('NounQuiz', 'noun_list')}
+                  'noun': ('NounQuiz', 'noun_dict')}
 
     types = {'verb': ('definition', 'present', 'future'),
              'noun': ('demo1', 'demo2')}  # not yet implemented
@@ -186,131 +204,74 @@ class QuizSetup():
         return QuizInterface(quiz_data, quiz_specs)
 
 
-# Maybe score tracking should be broken into its own class
-# TODO: inherit from a User object to track persistant quiz history
+# TODO: pass Quiz and User Instances as args
 class QuizInterface():
-    """ Handles User-facing quiz IO and scoretracking """
+    """ Handles User-facing quiz IO and scoretracking
 
-    def __init__(self, quiz_data, quiz_specs=None):
-        """ Param: quiz_data - created by [Subject]Quiz object
-        (question_key, answer, definition, question_str)
+    Params:
+        quiz - instantiated Quiz object via QuizSetup
+        user - instantiated User object of user taking the quiz
+    """
 
-        Param: quiz_type: Quiz.quiz_string"""
+    def __init__(self, quiz):
 
-        self.quiz_data = quiz_data
-        self.quiz_specs = quiz_specs
-        self.quiz_length = len(quiz_data)
-        self.num_correct = 0
-        self.num_wrong = 0
-
-    def start_quiz(self):
-        """ Loop through quiz_data and return results tuple.
-        return (quiz_type, num_correct, num_wrong, timestamp) """
-        for q in self.quiz_data:
-            self.print_feedback(self.feedback(self.ask_question(q)))
-        # TODO: implement the quiz_type return value
-        return (self.quiz_specs, self.num_correct, self.num_wrong,
-                datetime.utcnow())
-
-    def ask_question(self, q):
-        """ Returns answer_result boolean, increments num_correct / num_wrong
-        based on user input
-
-        Param: q = (question_key, answer, definition, question_str) """
-
-        answer = q[1]
-        user_answer = self.get_input(q[3])
-        answer_result = self.check_answer(q[1], user_answer)
-        self.update_score(answer_result)
-        return (answer_result, answer)
-
-    def feedback(self, answer_results):
-        """ Returns feedback string to be printed by print_feedback
-        Param: answer_results tuple
-        [0] = True if correct / False if incorrect
-        [1] = correct answer """
-
-        meta_data = '{:.0%} correct with {} question{} remaining.'.format(
-                    self.score_percent, self.questions_remaining,
-                    '' if self.questions_remaining == 1 else 's')
-        if answer_results[0]:
-            return('\nCorrect! ' + meta_data + '\n')
-        else:
-            return('\nHmm not quite. The correct answer is {}.\n'.format(
-                answer_results[1]) + meta_data + '\n')
-
-    @staticmethod
-    def print_feedback(feedback_string):
-        print(feedback_string)
+        self.quiz = quiz
 
     @staticmethod
     def get_input(question_string):
         """ Prompt user for answer input. Returns user_answer string """
         return input(question_string + '  ')
 
-    # implementing in Question , remove from here
-    @staticmethod
-    def check_answer(answer, user_answer):
-        """ Return True if user input answer is correct """
-        # this is for definition precision flexibility
-        return user_answer in answer and len(user_answer) >= (len(answer) / 3)
+    # TODO: update to use question object DONE
+    def ask_question(self, question):
+        """ Returns question object with .correct attribute updated,
+            increments quiz.num_correct / num_wrong based on user input
 
-    # can this be better implemented with setters, getters?
-    def update_score(self, boolean):
-        """ Receives Boolean, updates num_correct(True) / num_wrong(False) """
-        if not isinstance(boolean, bool):
-            return False
-        if boolean:
-            self.num_correct += 1
-        elif not boolean:
-            self.num_wrong += 1
-        return True
+        Param: question - instance of Question
+        """
 
-    @property
-    def score_percent(self):
-        return round(self.num_correct / self.questions_asked, 2)
+        user_answer = self.get_input(question.question)
+        answer_result = question.check(question.answer, user_answer)
+        self.quiz.update_score(answer_result)
+        return question
 
-    @property
-    def questions_asked(self):
-        return self.num_correct + self.num_wrong
+    def feedback(self, question):
+        """ Returns feedback string to be printed by print_feedback
 
-    @property
-    def questions_remaining(self):
-        return self.quiz_length - (self.num_correct + self.num_wrong)
+        Param:
+            question object with updated results via self.ask_question
+        """
 
-    @property
-    def quiz_string(self):
-        """ Return quiz_string for printing based on quiz_specs """
-        if self.quiz_specs[1] == 'definition':
-            return f'{self.quiz_specs[0]} {self.quiz_specs[1]} quiz'
+        meta_data = '{:.0%} correct with {} question{} remaining.'.format(
+                    self.quiz.score_percent, self.quiz.questions_remaining,
+                    '' if self.quiz.questions_remaining == 1 else 's')
+
+        if question.correct:
+            return('\nCorrect! ' + meta_data + '\n')
         else:
-            return f'{self.quiz_specs[1]} tense {self.quiz_specs[0]} quiz'
+            return('\nHmm not quite. The correct answer is {}.\n'.format(
+                question.answer) + meta_data + '\n')
 
-    def print_results(self, results):
+    @staticmethod
+    def print_feedback(feedback_string):
+        print(feedback_string)
+
+    def start_quiz(self):
+        """ Loop through quiz.question_list, receiving and giving user IO.
+            Returns updated quiz object
+        """
+
+        for q in self.quiz.question_list:
+            self.print_feedback(self.feedback(self.ask_question(q)))
+
+        self.quiz.date_taken = datetime.utcnow()
+        # this returned quiz object could later be passed to db object
+        return self.quiz
+
+    def print_results(self):
         print('You completed a {} on {}.\n\n\
             You got {} question{} correct and {} question{} wrong.\n'.format(
-            self.quiz_string, results[3].strftime('%x'),
-            results[1], '' if results[1] == 1 else 's',
-            results[2], '' if results[2] == 1 else 's'))
+            self.quiz.__str__, self.quiz.date_taken.strftime('%x'),
+            self.quiz.num_correct, '' if self.quiz.num_correct == 1 else 's',
+            self.quiz.num_wrong, '' if self.quiz.num_wrong == 1 else 's'))
         return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
