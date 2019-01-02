ac0c8f0c247e434915c614003563684a6d3dd63d
aherzfeld <herzfeld.andrew@gmail.com>
2019-01-03 02:24:52 +0800

    created models & removed db experimental methods

--- chingu/db.py
+++ chingu/db.py
@@ -1,20 +1,18 @@
-# SQLAlchemy core database implementation
-from datetime import datetime
+""" Define Database (factory class & DBInterface to handle CRUD  """
 from sqlalchemy import create_engine
-from sqlalchemy import (Table, Column, Integer, String, MetaData, ForeignKey,
-                        DateTime, Boolean)
 
 
 class Database():
     # http://docs.sqlalchemy.org/en/latest/core/engines.html
     DB_ENGINE = {
-        'sqlite': 'sqlite:///{DB}'
+        'sqlite': 'sqlite:///{DB}',
     }
 
     # Main DB connection reference object
     db_engine = None
 
-    def __init__(self, dbtype, username='', password='', dbname=''):
+    def __init__(self, dbtype, username='', password='', dbname=':memory:'):
+        """ Create database engine based on given parameters """
         dbtype = dbtype.lower()
         if dbtype in self.DB_ENGINE.keys():
             engine_url = self.DB_ENGINE[dbtype].format(DB=dbname)
@@ -23,122 +21,5 @@ class Database():
         else:
             print("DBType is not found in DB_ENGINE")
 
-    def create_db_tables(self):
-        metadata = MetaData()
 
-        # when switching to ORM add Column('quizzes', ),
-        # maybe just query for num_correct / wrong
-        user = Table('user', metadata,
-                     Column('id', Integer, primary_key=True),
-                     Column('username', String, nullable=False),
-                     Column('date_created', String))
-                     # Column('num_correct', Integer),
-                     # Column('num_wrong', Integer))
-        # when switching to ORM add Column('questions', ),
-        quiz = Table('quiz', metadata,
-                     Column('id', Integer, primary_key=True),
-                     Column('category', String, nullable=False),
-                     Column('type', String, nullable=False),
-                     # These can be retrieved via queries
-                     # Column('num_correct', Integer),
-                     # Column('num_wrong', Integer),
-                     Column('date_taken', DateTime),
-                     Column('user_id', Integer, ForeignKey('user.id')))
-        question = Table('question', metadata,
-                         Column('id', Integer, primary_key=True),
-                         Column('key', String),
-                         Column('answer', String),
-                         Column('definition', String),
-                         Column('correct', Boolean),
-                         Column('quiz_id', Integer, ForeignKey('quiz.id')))
-        try:
-            metadata.create_all(self.db_engine)
-            print('Tables created.')
-        except Exception as e:
-            print('An error occurred during table creation.')
-            print(e)
-
-    # Insert, Update, Delete
-    def execute_query(self, query=''):
-        """ Performs provided SQL query """
-        if query == '':
-            return
-        print(query)
-        with self.db_engine.connect() as connection:
-            try:
-                connection.execute(query)
-            except Exception as e:
-                print(e)
-
-    def drop_table(self, table):
-        self.execute_query("DROP TABLE {};".format(table))
-        print('Dropped {} table.'.format(table))
-
-    def print_all_data(self, table='', query=''):
-        """ Prints all data from given table or executes optional query """
-        if query != '':
-            query = query
-        else:
-            query = "SELECT * FROM '{}';".format(table)
-        print(query)
-        with self.db_engine.connect() as connection:
-            try:
-                result = connection.execute(query)
-            except Exception as e:
-                print(e)
-            else:
-                for row in result:
-                    print(row)
-                result.close()
-        print('\n')
-
-
-# figuring out if I should load user/quiz ids upon instatiation
-class DB_Interface():
-    """ Instantiate with Database, User & Quiz objects to update data """
-
-    def __init__(self, db, user, quiz):
-        self.db = db
-        self.user = user
-        self.quiz = quiz
-
-    @property
-    def user_id(self):
-        return self.db.execute_query(f"SELECT id FROM user WHERE \
-username={self.user.username});")
-
-    @property
-    def quiz_id(self):
-        return self.db.execute_query(f"SELECT id FROM quiz WHERE \
-date_taken={self.quiz.date_taken});")
-
-    def add_user(self):
-        self.db.execute_query("INSERT INTO user (username, date_created)\
-VALUES ('{}', '{}');".format(self.user.username, datetime.utcnow()))
-
-    # is this even needed?? might be reverse to pull data from db in User
-    def update_user(self):
-        """ Use to update user data after completing quiz """
-        pass
-
-    def update_quiz(self):
-        """ Use to update quiz data after completing quiz """
-        self.execute_query(f"INSERT INTO quiz (category, type, date_taken,\
-user_id) VALUES(\
-{self.quiz.category} {self.quiz.type} {self.quiz.date_taken} {self.user_id});")
-
-    def update_question(self):
-        """ Use to update question data after completing quiz """
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
+# TODO: create DB_Interface
---
+++ chingu/models.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import (Column, Integer, String, ForeignKey, Boolean)
+
+
+Base = declarative_base()
+
+
+class Question(Base):
+    __tablename__ = 'questions'
+
+    question_id = Column('id', Integer, primary_key=True)
+    key = Column(String)
+    answer = Column(String)
+    definition = Column(String)
+    question = Column(String)
+    correct = Column(Boolean)
+    quiz_id = Column(Integer, ForeignKey('quizzes.id'))
+
+    quiz = relationship('Quiz', back_populates='questions')
+
+    def __repr__(self):
+        return (f"<Question(key='{self.key}', answer='{self.answer}', "
+                f"definition='{self.definition}', question='{self.question}', "
+                f"quiz_id={self.quiz_id})>")
+
+
+class Quiz(Base):
+    __tablename__ = 'quizzes'
+
+    quiz_id = Column('id', Integer, primary_key=True)
+    category = Column(String, nullable=False)
+    quiz_type = Column('type', String, nullable=False)
+    taken_on = Column(String,
+                      default=datetime.utcnow().isoformat(' ', 'seconds'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    questions = relationship('Question', order_by=Question.question_id,
+                             back_populates='quiz')
+    user = relationship('User', back_populates='quizzes')
+
+    def __repr__(self):
+        return (f"<Quiz(category='{self.category}', "
+                f"quiz_type='{self.quiz_type}', "
+                f"quiz_id={self.quiz_id}>)")
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column('id', Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    # String because sqlite has no DateTime type isoformat='YYYY-MM-DD'
+    created_on = Column(String,
+                        default=datetime.utcnow().isoformat(' ', 'seconds'))
+    # num_correct , num_wrong
+
+    quizzes = relationship('Quiz', order_by=Quiz.quiz_id,
+                           back_populates='user')
+
+    def __repr__(self):
+        return (f"<User(user_id={self.user_id}, username='{self.username}',"
+                f" created_on={self.created_on})")
