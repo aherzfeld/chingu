1f9b695b1ac1e7cdfbf29cde974351951d3f0b3d
aherzfeld <herzfeld.andrew@gmail.com>
2019-01-24 16:46:07 +0800

    added password reset emails templates

--- chingu/auth/routes.py
+++ chingu/auth/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
-
 from chingu import db
 from chingu.auth import bp
 from chingu.auth.forms import (LoginForm, RegistrationForm,
