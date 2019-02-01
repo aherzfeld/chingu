06c6101068db8e523381c476576442bffc3f04ef
aherzfeld <herzfeld.andrew@gmail.com>
2019-02-01 15:25:40 +0800

    converted quiz_setup form to dropdowns

--- chingu/core/forms.py
+++ chingu/core/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import SelectField, SubmitField, IntegerField
 from wtforms.validators import AnyOf, DataRequired, NumberRange
 
 
 class QuizSetupForm(FlaskForm):
-    category = StringField('Category', validators=[DataRequired(),
-        AnyOf(['verb'])])
-    quiz_type = StringField('Quiz Type', validators=[DataRequired(),
-        AnyOf(['definition', 'present'])])
+    category = SelectField('Category', choices=[('verb', 'Verb')],
+        validators=[DataRequired()])
+    quiz_type = SelectField('Quiz Type', choices=[('definition', 'Definition'),
+        ('present', 'Present Tense')], validators=[DataRequired()])
     length = IntegerField('Length', validators=[DataRequired(),
                                                 NumberRange(min=1, max=20)])
     submit = SubmitField('Start Quiz')
--- chingu/core/routes.py
+++ chingu/core/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_required
 #  from chingu.core.forms import
 #  from chingu.models import
 from chingu.core import bp
+from chingu.core.forms import QuizSetupForm
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -13,6 +14,7 @@ from chingu.core import bp
 def index():
     return render_template('index.html')
 
-@bp.route('quiz_setup', methods=['GET', 'POST'])
+@bp.route('/quiz_setup', methods=['GET', 'POST'])
 def quiz_setup():
-    return render_template('quiz_setup.html')
+    form = QuizSetupForm()
+    return render_template('quiz_setup.html', form=form)
