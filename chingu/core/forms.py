20b9b7f34bf0fb7277e9cf0def9c6556451a6831
aherzfeld <herzfeld.andrew@gmail.com>
2019-02-05 17:45:48 +0800

    refactored core.forms for PEP8 compliance

--- chingu/core/forms.py
+++ chingu/core/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, SubmitField, IntegerField
-from wtforms.validators import AnyOf, DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange
 
 
 class QuizSetupForm(FlaskForm):
     category = SelectField('Category', choices=[('verb', 'Verb')],
-        validators=[DataRequired()])
+                           validators=[DataRequired()])
     quiz_type = SelectField('Quiz Type', choices=[('definition', 'Definition'),
-        ('present', 'Present Tense')], validators=[DataRequired()])
+                                                  ('present', 'Present Tense')],
+                            validators=[DataRequired()])
     length = IntegerField('Length', validators=[DataRequired(),
                                                 NumberRange(min=1, max=20)])
     submit = SubmitField('Start Quiz')
